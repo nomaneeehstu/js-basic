a9009376ab9ab5bce2fc3ce11ab084e5e89e8338
nomaneeehstu <nomanhstu18@gmail.com>
2021-09-01 18:57:12 +0600

    js

---
+++ basic.js
@@ -0,0 +1,17 @@
+// console.log(3);
+var a = '23';
+ var b = '33';
+ var bnum = parseInt(b);
+console.log(a);
+
+var firstName ='Md. Abdullah';
+var lastName ='Al Noamn';
+var fullName = firstName + ' ' + lastName;
+
+// console.log(fullName);
+
+var pass = true;
+// console.log(pass);
+var shoesPrice = '499.99';
+var pantPrice = '999.99';
+
